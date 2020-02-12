ce34df99ddf16f137e0092d8f1c5f4652fab145b
Ale Mohamad <hello@alemohamad.com>
2020-02-12 15:01:07 +0100

    Extended admin features

--- libros/apps/biblioteca/admin.py
+++ libros/apps/biblioteca/admin.py
@@ -3,5 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Book
 
-admin.site.register(Author)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'nationality',
+        'id'
+    )
+    search_fields = ('name',)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'year',
+        'author',
+        'id'
+    )
+    search_fields = ('title',)
+    list_filter = ('author',)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
