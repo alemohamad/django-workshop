75cb1e527590642a09cf4e84d4e3494aacaa800e
Ale Mohamad <hello@alemohamad.com>
2020-02-12 14:52:57 +0100

    Created Author model and migration

--- libros/apps/biblioteca/models.py
+++ libros/apps/biblioteca/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField('Nombre', max_length=100)
+    nationality = models.CharField('Nacionalidad', max_length=100)
+
+    def __str__(self):
+        return self.name
