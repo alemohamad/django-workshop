84350e1b3649c6151766b4345f47ea2c39b7bd12
Ale Mohamad <hello@alemohamad.com>
2020-02-12 14:49:35 +0100

    Updated to ListView and added a queryset

--- libros/apps/biblioteca/views.py
+++ libros/apps/biblioteca/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'biblioteca/index.html'
+    queryset = ['Cien años de soledad', 'Harry Potter', 'El principito', 'Crónica de una muerte anunciada']
+    context_object_name = 'books'
